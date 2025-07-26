9a44bba41c498a0f4475e518fac24bff781fdba0
Placeless <thinkoverheat@gmail.com>
2025-07-26 23:47:49 +0900

    URL 判定箇所実装

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -11,15 +11,9 @@ pub struct Url {
 
 use alloc::string::ToString;
 
-impl Url { // コンストラクタ内で文字列予約しておく
-  pub fn parse(&mut self) -> Result<Self, String> { 
-    // 自己可変参照により new 関数で作成した空文字のフィールドを変更可能
-    {
-      Ok(self.clone()) // 次のセクション以降で実装
-    }
-  }
-  
-  pub fn new(url: String) -> Self {
+impl Url { 
+    
+  pub fn new(url: String) -> Self { // コンストラクタ内で文字列予約しておく
     Self {
       url,
       host:"".to_string(),
@@ -28,4 +22,22 @@ impl Url { // コンストラクタ内で文字列予約しておく
       searchpart:"".to_string(),
     }
   }
+
+  fn is_http(&mut self) -> bool {
+    if self.url.contains("http://") {
+      return true;
+    }
+    false
+  }
+
+  pub fn parse(&mut self) -> Result<Self, String> { 
+    // 自己可変参照により new 関数で作成した空文字のフィールドを変更可能
+    if !self.is_http() { // まず上の関数でURLの頭を判定する
+      return Err("Only HTTP scheme is supported".to_string());
+    }
+    {
+      Ok(self.clone()) // 次のセクション以降で実装
+    }
+  }
+
 }

3fce01b12fd79a247282daf023b0c6c546902da9
Placeless <thinkoverheat@gmail.com>
2025-07-29 06:07:05 +0900

    正誤表対応

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use noli::prelude::*;
 
 fn main() -> u64 {
     let client = HttpClient::new();
-    match client.get("example.com".to_string(), 80, "/".to_string()) {
+    match client.get("example.com".to_string(), 80, "".to_string()) {
         Ok(res) => {
             print!("response:\n{:#?}", res);
         }
